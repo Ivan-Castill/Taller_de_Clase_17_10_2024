e514760a0d41d4611a26e3896a4d2a8593135c1d
Ivan-Castill <ivan2003castillo12345@gmail.comzx>
2024-10-17 09:43:37 -0500

    New Version

--- src/ClaseJugador.java
+++ src/ClaseJugador.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class ClaseJugador {
+    String avatar;
+    int edad;
+    String pais;
+
+    public ClaseJugador(String avatar,int edad,String pais) {
+        this.avatar = avatar;
+        this.edad= edad;
+        this.pais=pais;
+    }
+    public ClaseJugador() {
+        avatar = "Ivan206";
+        edad= 20;
+        pais= "ECUADOR";
+    }
+    public void patar(){
+        System.out.println("JUGADOR 1");
+        System.out.println("-------------------------------");
+        System.out.println("El Avatar es: "+avatar);
+        System.out.println("La Edad del jugador es: "+edad);
+        System.out.println("El pais del jugador es: "+pais);
+        System.out.println("El jugador patea.");
+        System.out.println("-------------------------------");
+    }
+    public void correr(){
+        System.out.println("JUGADOR 1");
+        System.out.println("-------------------------------");
+        System.out.println("El Avatar es: "+avatar);
+        System.out.println("La Edad del jugador es: "+edad);
+        System.out.println("El pais del jugador es: "+pais);
+        System.out.println("El jugador corre.");
+        System.out.println("-------------------------------");
+    }
+    public void saltar(){
+        System.out.println("JUGADOR 2");
+        System.out.println("-------------------------------");
+        System.out.println("El Avatar es: "+avatar);
+        System.out.println("La Edad del jugador es: "+edad);
+        System.out.println("El pais del jugador es: "+pais);
+        System.out.println("El jugador salta.");
+        System.out.println("-------------------------------");
     }
 }
--- src/LlamarClase.java
+++ src/LlamarClase.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class LlamarClase {
+    public static void main(String[] args){
+        ClaseJugador one =new ClaseJugador("Juan", 25,"Estados Unidos");
+        ClaseJugador two =new ClaseJugador("Richi", 20,"Mexico");
+        ClaseJugador three =new ClaseJugador("Walter", 19,"Peru");
+        ClaseJugador four =new ClaseJugador("Pedro", 30,"Colombia");
+        ClaseJugador five =new ClaseJugador("Luiz", 24,"Argetina");
+        ClaseJugador six =new ClaseJugador();
+
+        one.patar();
+        two.correr();
+        three.saltar();
+        four.saltar();
+        five.correr();
+        six.patar();
+    }
 }
